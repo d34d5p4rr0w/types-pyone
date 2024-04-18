9cd50f925758ea909454638bcbb52c596911d45e
d34d5p4rr0w <16529603+d34d5p4rr0w@users.noreply.github.com>
2024-04-18 12:00:16 +0300

    Increase typing coverage
    Signed-off-by: d34d5p4rr0w <16529603+d34d5p4rr0w@users.noreply.github.com>

--- src/pyone/__init__.pyi
+++ src/pyone/__init__.pyi
@@ -1,11 +1,12 @@
 # Stub file for pyone module
 import xmlrpc.client
-from typing import Any, Dict, Optional, Tuple, Union
+from typing import Any, Dict, Optional, Tuple, Union, Protocol
 from xmlrpc.client import Transport, _Marshallable
 
 from _typeshed import SizedBuffer
 from aenum import IntEnum
 from six import string_types
+from . import bindings
 
 # Exceptions as defined in the XML-API reference
 class OneException(Exception): ...
@@ -38,6 +39,19 @@ PAGINATED_POOLS: IntEnum
 REMOVE_VNET_ATTRS: IntEnum
 VM_STATE: IntEnum
 
+class OneImagePoolMethod(Protocol):
+    def info(self, resources: int, begin: int, end: int) -> bindings.IMAGE_POOLSub: ...
+
+
+class OneTemplateMethod(Protocol):
+    def instantiate(self, template_id: int, name: str, hold: bool, extra: str) -> int: ...
+
+
+class OneVMMethod(Protocol):
+    def info(self, vm_id: int) -> bindings.VMSub: ...
+    def action(self, name: str, vm_id: int) -> None: ...
+
+
 class OneServer(xmlrpc.client.ServerProxy):
     def __init__(self, uri: str, session: str, timeout: Optional[int] = None, https_verify: bool = True, **options: Any): ...
     def _ServerProxy__request(self, methodname: str, params: Tuple) -> Any: ...
@@ -46,6 +60,15 @@ class OneServer(xmlrpc.client.ServerProxy):
     def __response(self, raw_response: Any) -> Any: ...
     def server_retry_interval(self) -> int: ...
     def server_close(self) -> None: ...
+
+    @property
+    def imagepool(self) -> OneImagePoolMethod: ...
+
+    @property
+    def template(self) -> OneTemplateMethod: ...
+
+    @property
+    def vm(self) -> OneVMMethod: ...
 
 class RequestsTransport(xmlrpc.client.Transport):
     user_agent: str
